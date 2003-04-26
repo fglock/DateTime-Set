7dd6ec35ee3398da775894e775a942aabf2b5f12
fglock <fglock@49043108-e40d-0410-ab17-85caa8b5b18d>
2003-04-26 04:01:43 +0000

    updated version number, dependencies.
    git-svn-id: https://perl-date-time.svn.sourceforge.net/svnroot/perl-date-time/modules/DateTime-Set@1432 49043108-e40d-0410-ab17-85caa8b5b18d

--- trunk/lib/DateTime/Set.pm
+++ trunk/lib/DateTime/Set.pm
@@ -13,7 +13,7 @@ $Set::Infinite::PRETTY_PRINT = 1;   # enable Set::Infinite debug
 
 use vars qw( $VERSION );
 
-$VERSION = '0.02';
+$VERSION = '0.03';
 
 use constant INFINITY     =>       100 ** 100 ** 100 ;
 use constant NEG_INFINITY => -1 * (100 ** 100 ** 100);
