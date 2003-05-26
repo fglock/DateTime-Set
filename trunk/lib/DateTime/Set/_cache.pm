aa2e04e7dbd3795eb7425076de25c241ed03dca3
fglock <fglock@49043108-e40d-0410-ab17-85caa8b5b18d>
2003-05-26 18:54:38 +0000

    vaporware docs
    git-svn-id: https://perl-date-time.svn.sourceforge.net/svnroot/perl-date-time/modules/DateTime-Set@1480 49043108-e40d-0410-ab17-85caa8b5b18d

--- trunk/lib/DateTime/Set/_cache.pm
+++ trunk/lib/DateTime/Set/_cache.pm
@@ -1,9 +1,29 @@
 package DateTime::Set::_cache;
 
 use strict;
+use Params::Validate qw( validate SCALAR BOOLEAN HASHREF OBJECT );
 
-# blah blah blah
-# the actual implementation is yet under discussion in datetime@perl.org
+# This is mostly a placeholder.
+# The actual implementation is yet under discussion in datetime@perl.org
+
+# The cache() method must build an actual set-object,
+# such that cacheing is transparent to the user.
+#
+# default cache is a simple in-memory hash
+#
+sub cache {
+    my $self = shift;
+    my $class = ref($self);
+    die "Object must be a Set" unless UNIVERSAL::can( $self, 'union' );
+
+    my %p = validate( @_,
+                      { cache  => { type => OBJECT },
+                      }
+                    );
+
+    # TODO!
+    die "not implemented";
+}
 
 1;
 
@@ -26,6 +46,40 @@ through Cache::Cache API.
 This module implements internal cacheing routines common to DateTime::Set 
 and DateTime::SpanSet.
 
+=head1 METHODS
+
+=over 4 
+
+=item * to_cache
+
+Stores a set (non-recurrence) into a cache.
+
+     $set->to_cache( cache => $cache, key => 'my_set' );
+
+"key" is optional.
+
+C<to_cache> will die if the set is a recurrence.
+
+=item * from_cache
+
+Retrieves a set (non-recurrence) from a cache.
+
+     $set = DateTime::Set->from_cache( cache => $cache, key => 'my_set' );
+
+"key" is optional.
+
+C<from_cache> will die if the cached set is a recurrence.
+
+=item * cache
+
+Associates a cache to a recurrence set.
+
+     $cached_set = $recurrence_set->cache( cache => $cache, key => 'my_set' );
+
+"key" is optional.
+
+=back
+
 =head1 AUTHOR
 
 Flavio S. Glock <fglock@pucrs.br>
@@ -40,6 +94,8 @@ The full text of the license can be found in the LICENSE file included
 with this module.
 
 =head1 SEE ALSO
+
+Cache::Cache
 
 datetime@perl.org mailing list
 
