3f575754be4e7d84cd7c36771b0b8cc0966b1af2
autarch <autarch@49043108-e40d-0410-ab17-85caa8b5b18d>
2003-08-29 17:42:05 +0000

    use new subtract_datetime_absolute method so that duration is always accurate
    git-svn-id: https://perl-date-time.svn.sourceforge.net/svnroot/perl-date-time/modules/DateTime-Set@1508 49043108-e40d-0410-ab17-85caa8b5b18d

--- trunk/lib/DateTime/Span.pm
+++ trunk/lib/DateTime/Span.pm
@@ -280,7 +280,8 @@ sub end_is_closed { $_[0]->end_is_open ? 0 : 1 }
 # span == $self
 sub span { @_ }
 
-sub duration { my $dur = $_[0]->end - $_[0]->start; defined $dur ? $dur : INFINITY }
+sub duration { my $dur = $_[0]->end->subtract_datetime_absolute( $_[0]->start );
+	       defined $dur ? $dur : INFINITY }
 *size = \&duration;
 
 # unsupported Set::Infinite methods
