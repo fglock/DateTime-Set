ec2fc652407b9267bf17770f80d7b26cfcbcd40e
fglock <fglock@49043108-e40d-0410-ab17-85caa8b5b18d>
2004-03-26 20:00:11 +0000

    version 0.1410
    git-svn-id: https://perl-date-time.svn.sourceforge.net/svnroot/perl-date-time/modules/DateTime-Set@1565 49043108-e40d-0410-ab17-85caa8b5b18d

--- trunk/lib/DateTime/Set.pm
+++ trunk/lib/DateTime/Set.pm
@@ -16,7 +16,7 @@ use constant INFINITY     =>       100 ** 100 ** 100 ;
 use constant NEG_INFINITY => -1 * (100 ** 100 ** 100);
 
 BEGIN {
-    $VERSION = '0.1409';
+    $VERSION = '0.1410';
 }
 
 sub iterate {
